01b4e875a2c9c68d257a826e4ff7857fc0b6546e
ashv2ley <ashley0425@yahoo.com>
2025-01-19 17:54:37 -0800

    finished mission statement section

--- VFSWebsite/src/components/Navbar.jsx
+++ VFSWebsite/src/components/Navbar.jsx
@@ -8,14 +8,14 @@ import {faDiscord} from "@fortawesome/free-brands-svg-icons";
 
 export default function Navbar() {
     return (
-        <div className="flex flex-row justify-between items-center lg:mx-10">
+        <div className="flex flex-row justify-between items-center md:mx-10">
             <img className="w-[140px]" src={Logo} alt="Vertical Flight Society Logo" />
             <div className="flex gap-12 text-grey">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/news">News</Link>
-                <Link to="/events">Events</Link>
-                <Link to="/contact">Contact</Link>
+                <Link to="/" className={"relative px-4 py-2 ease-in-out duration-300 hover:bg-grey/10 hover:px-4 hover:py-2 hover:rounded-sm"}>Home</Link>
+                <Link to="/about" className={"relative px-4 py-2 ease-in-out duration-300 hover:bg-grey/10 hover:px-4 hover:py-2 hover:rounded-sm"}>About</Link>
+                <Link to="/news" className={"relative px-4 py-2 ease-in-out duration-300 hover:bg-grey/10 hover:px-4 hover:py-2 hover:rounded-sm"}>News</Link>
+                <Link to="/events" className={"relative px-4 py-2 ease-in-out duration-300 hover:bg-grey/10 hover:px-4 hover:py-2 hover:rounded-sm"}>Events</Link>
+                <Link to="/contact" className={"relative px-4 py-2 ease-in-out duration-300 hover:bg-grey/10 hover:px-4 hover:py-2 hover:rounded-sm"}>Contact</Link>
             </div>
             <div className="flex gap-8 text-lg text-grey">
                 <FontAwesomeIcon icon={faInstagram} />
--- VFSWebsite/src/pages/Home.jsx
+++ VFSWebsite/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./HomeSections/Hero.jsx"
+import MissionStatement from "./HomeSections/MissionStatement.jsx";
 
 export default function Home(){
     return (
         <div>
             <Hero />
+            <MissionStatement />
         </div>
     )
 }
