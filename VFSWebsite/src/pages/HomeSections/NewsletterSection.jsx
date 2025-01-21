a7244584b296e9618f4cc5ae7ad22dd5961e80fe
ashv2ley <ashley0425@yahoo.com>
2025-01-21 11:41:22 -0800

    Fixed alignment

--- VFSWebsite/src/pages/HomeSections/NewsletterSection.jsx
+++ VFSWebsite/src/pages/HomeSections/NewsletterSection.jsx
@@ -9,15 +9,17 @@ export default function NewsletterSection() {
     return (
         <div className={" w-screen bg-red text-white flex flex-col items-center justify-center pb-16 gap-3"}>
             <img src={Logo} alt={"newsletter logo"} className={"w-36"} />
-            <h1 className={"text-2xl font-semibold"}>Love Vertical Flight Society?</h1>
-            <p className={"text-md"}>Join our newsletter below!</p>
-            <form>
-                <input
-                    placeholder={"Email"}
-                    type={"text"} className={"text-black rounded-sm p-2 pr-20"}
-                    onSubmit={handleSubmit}
-                    onChange={(e) => setEmail(e.target.value)} />
-            </form>
+            <div className={"flex flex-col gap-3 items-center justify-center"}>
+                <h1 className={"text-2xl font-semibold"}>Love Vertical Flight Society?</h1>
+                <p className={"text-md"}>Join our newsletter below!</p>
+                <form>
+                    <input
+                        placeholder={"Email"}
+                        type={"text"} className={"text-black rounded-sm p-2 pr-20"}
+                        onSubmit={handleSubmit}
+                        onChange={(e) => setEmail(e.target.value)}/>
+                </form>
+            </div>
         </div>
     )
 }
