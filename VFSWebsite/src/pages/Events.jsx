3c374b41ad6cc051ff6906c5c3d22e6fffc7ec93
tylerttrannn <tylerttrann@gmail.com>
2025-01-21 09:34:31 -0800

    Created event component + added events images

--- VFSWebsite/src/pages/Events.jsx
+++ VFSWebsite/src/pages/Events.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import Event from "./EventSections/Event";
 
-export default function Events(){
+export default function Events() {
     return (
-        <div>
-            Events
+        <div className="max-w-7xl mx-auto my-10">
+            {/* Past Events Heading */}
+            <h1 className="text-6xl font-extrabold pl-16 pt-8">Past Events</h1>
+
+            <div>
+                <Event />
+            </div>
+
         </div>
-    )
+    );
 }
