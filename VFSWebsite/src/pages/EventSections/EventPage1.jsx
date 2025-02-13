0f8588191fad0ac0b8809b2c5169223efd300954
tylerttrannn <tylerttrann@gmail.com>
2025-02-12 17:26:10 -0800

    modified events page for mobile view (need to add some form of dot navigation) for user

--- VFSWebsite/src/pages/EventSections/EventPage1.jsx
+++ VFSWebsite/src/pages/EventSections/EventPage1.jsx
@@ -87,55 +87,49 @@ VFS at UCI hosted Axel Radermacher, Co-Founder and Head of Product at Odys Aviat
 
   return (
       <div className="space-y-2 max-w-7xl mx-auto pb-10">
-          <h1 className="text-6xl font-extrabold pl-16 my-10">Past Events</h1>
-          {events.map((event, index) => (
-              <div
-                  key={index}
-                  // using flex-row-reverse to alternate image
-                  className={`flex flex-col md:flex-row ${
-                      index % 2 !== 0 ? "md:flex-row-reverse" : ""
-                  }  p-5 `}
-              >
+        <h1 className="text-3xl sm:text-6xl font-extrabold text-center sm:text-left sm:ml-16 my-10">Past Events</h1>
+        {events.map((event, index) => (
+              <div key={index} className={`flex flex-col md:flex-row ${index % 2 !== 0 ? "md:flex-row-reverse" : ""}  p-5 `}>
 
-                  {/* Image */}
-                  <div className="md:w-1/2 flex items-center justify-center relative">
-                      {event.images && event.images.length > 0 && (
-                          <Carousel className="relative w-full max-w-[65%]">
-                              <CarouselContent>
-                                  {event.images.map((image, index) => (
-                                      <CarouselItem key={index} className="flex justify-center">
-                                          <img src={image} className="rounded-lg h-[320px] "
-                                               alt={`Slide ${index + 1}`}/>
-                                      </CarouselItem>
-                                  ))}
-                              </CarouselContent>
+                <div className="md:w-1/2 w-full flex items-center justify-center relative pb-5">
+                {event.images && event.images.length > 0 && (
+                    <Carousel className="relative w-full sm:max-w-[65%]">
+                    <CarouselContent>
+                        {event.images.map((image, index) => (
+                        <CarouselItem key={index} className="flex justify-center">
+                            <img
+                            src={image}
+                            className="rounded-lg w-full sm:h-[320px] h-[350px] object-contain"
+                            alt={`Slide ${index + 1}`}
+                            />
+                        </CarouselItem>
+                        ))}
+                    </CarouselContent>
 
-                              {/*modifying the styling of the buttons to make it more compact*/}
-                              <div className="absolute inset-0 flex justify-between items-center px-4">
-                                  <CarouselPrevious
-                                      className="bg-gray-800/70 rounded-full p-2 hover:bg-gray-900 transition w-10 h-10 flex items-center justify-center"/>
-                                  <CarouselNext
-                                      className="bg-gray-800/70 rounded-full p-2 hover:bg-gray-900 transition w-10 h-10 flex items-center justify-center"/>
-                              </div>
-                          </Carousel>
-                      )}
-                  </div>
+                    {/* Modifying the styling of the buttons to make them more compact */}
+                    <div className="hidden sm:block absolute inset-0 flex justify-between items-center px-4">
+                        <CarouselPrevious className="bg-gray-800/70 rounded-full p-2 hover:bg-gray-900 transition w-10 h-10 flex items-center justify-center" />
+                        <CarouselNext className="bg-gray-800/70 rounded-full p-2 hover:bg-gray-900 transition w-10 h-10 flex items-center justify-center" />
+                    </div>
+                    </Carousel>
+                )}
+                </div>
 
-                  {/* Description of Event */}
-                  <div className="md:w-1/2 flex flex-col justify-center p-10">
-                      <div className="px-4">
-                          <div>
-                              <h1 className="text-[#C30D3B] font-bold text-4xl">{event.title}</h1>
-                              <h2 className="text-2xl font-bold mt-2">{event.date}</h2>
-                          </div>
-                          <div className="max-w-full mt-4">
-                              <p className="">
-                                  {event.description}
-                              </p>
-                          </div>
-                      </div>
+                {/* Description of Event */}
+                <div className="md:w-1/2 flex flex-col justify-center sm:p-10 ">
+                    <div className="px-4">
+                        <div>
+                            <h1 className="text-[#C30D3B] font-bold text-3xl sm:text-4xl">{event.title}</h1>
+                            <h2 className="text-2xl font-bold mt-2">{event.date}</h2>
+                        </div>
+                        <div className="max-w-full mt-4">
+                            <p className="text-sm md:text-md">
+                                {event.description}
+                            </p>
+                        </div>
+                    </div>
+                </div>
 
-                  </div>
               </div>
           ))}
 
