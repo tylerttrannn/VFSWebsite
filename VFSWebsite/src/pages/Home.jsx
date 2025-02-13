5951e0399673b4f461946bce3a20de44df726b52
tylerttrannn <tylerttrann@gmail.com>
2025-02-12 17:52:17 -0800

    hiding overflow on front page for mobile view

--- VFSWebsite/src/pages/Home.jsx
+++ VFSWebsite/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import HeroSection from "./HomeSections/HeroSection.jsx"
 import MissionStatement from "./HomeSections/MissionStatement.jsx";
 import ProjectSection from "./HomeSections/ProjectSection.jsx";
@@ -5,10 +6,11 @@ import TeamSection from "./HomeSections/TeamSection.jsx";
 import NewsletterSection from "./HomeSections/NewsletterSection.jsx";
 import ContactSection from "./HomeSections/ContactSection.jsx";
 import Divider from "../components/Divider.jsx";
+import Footer from "../components/Footer.jsx"
 import UpcomingEvents from "./HomeSections/UpcomingEvents.jsx";
 export default function Home() {
     return (
-        <div className="">
+        <div className="overflow-x-hidden">
             <HeroSection/>
             <Divider/>
             <MissionStatement/>
