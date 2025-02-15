b80da802ab4dc587a3491814377e60e2b8288783
tylerttrannn <tylerttrann@gmail.com>
2025-02-15 13:57:12 -0800

    fixed user cards on smaller screens + modified netlify config file for react router

--- VFSWebsite/src/pages/Home.jsx
+++ VFSWebsite/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import HeroSection from "./HomeSections/HeroSection.jsx"
 import MissionStatement from "./HomeSections/MissionStatement.jsx";
 import ProjectSection from "./HomeSections/ProjectSection.jsx";
@@ -6,11 +5,10 @@ import TeamSection from "./HomeSections/TeamSection.jsx";
 import NewsletterSection from "./HomeSections/NewsletterSection.jsx";
 import ContactSection from "./HomeSections/ContactSection.jsx";
 import Divider from "../components/Divider.jsx";
-import Footer from "../components/Footer.jsx"
 import UpcomingEvents from "./HomeSections/UpcomingEvents.jsx";
 export default function Home() {
     return (
-        <div className="overflow-x-hidden">
+        <div className="">
             <HeroSection/>
             <Divider/>
             <MissionStatement/>
