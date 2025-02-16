a8bdcc228cd89d2a7e3b05fadde9ccc934e99eb1
ashv2ley <ashley0425@yahoo.com>
2025-02-15 18:32:37 -0800

    added favicon and edited news section

--- VFSWebsite/src/pages/News.jsx
+++ VFSWebsite/src/pages/News.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 export default function News(){
     return (
-        <div className="text-black">
-            <iframe src={"https://verticalmag.com/email/newsletters/vertical-dn.html"} width={"100%"} height={"730px"}></iframe>
+        <div className="flex justify-center p-4 overflow-hidden">
+            <div className="w-full max-w-5xl h-[75vh] sm:h-[80vh] md:h-[85vh] lg:h-[90vh]">
+                <iframe
+                    src="https://verticalmag.com/email/newsletters/vertical-dn.html"
+                    className="w-full h-full rounded-lg shadow-lg border"
+                    style={{border: "none", overflow: "hidden"}}
+                ></iframe>
+            </div>
         </div>
     )
 }
